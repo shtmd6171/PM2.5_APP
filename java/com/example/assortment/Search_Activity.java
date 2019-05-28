d13aeebd3cf3104eff907a094cb234ea2fab093a
shtmd6171 <49226314+shtmd6171@users.noreply.github.com>
2019-05-28 23:09:17 +0900

    레이아웃 재구성

---
+++ java/com/example/assortment/Search_Activity.java
@@ -0,0 +1,145 @@
+package com.example.assortment;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class Search_Activity extends AppCompatActivity {
+
+    EditText edit;
+    TextView text;
+
+    XmlPullParser xpp;
+    String key = "iOsw4MlgRU0JZpvuR5AkLUfkX%2FAOl0Q03HF78VRzR2g0dz6iD0esiw6HmLHKly6PVvGVP2PPgRpqtpULJBWSHg%3D%3D";
+    String data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+       // edit = (EditText) findViewById(R.id.find_box);
+        //text = (TextView) findViewById(R.id.list);
+    }
+   /*
+    public void mOnClick(View v) {
+        switch (v.getId()) {
+            case R.id.find_btn:
+
+                //Android 4.0 이상 부터는 네트워크를 이용할 때 반드시 Thread 사용해야 함
+                new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        data = getXmlData();//아래 메소드를 호출하여 XML data를 파싱해서 String 객체로 얻어오기
+
+                        //UI Thread(Main Thread)를 제외한 어떤 Thread도 화면을 변경할 수 없기때문에
+                        //runOnUiThread()를 이용하여 UI Thread가 TextView 글씨 변경하도록 함
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // TODO Auto-generated method stub
+                                text.setText(data); //TextView에 문자열  data 출력
+                            }
+                        });
+                    }
+                }).start();
+                break;
+        }
+    }//mOnClick method..
+
+    String getXmlData() {
+
+        StringBuffer buffer = new StringBuffer();
+
+        String str = edit.getText().toString();//EditText에 작성된 Text얻어오기
+        String location = URLEncoder.encode(str);//한글의 경우 인식이 안되기에 utf-8 방식으로 encoding..
+        String queryUrl
+                = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?" + "stationName="
+                + location + "&dataTerm=month&pageNo=1&numOfRows=10&ServiceKey=9HdsYPXrtfkswuTTa%2BWLZXU2C1FgJT1wRMguaTKm3h4g1iUGfTrgvFjCheFxbb6acKR7dzoumtwhD6pyHhxd0Q%3D%3D&ver=1.3";
+
+
+        try {
+            URL url = new URL(queryUrl);//문자열로 된 요청 url을 URL 객체로 생성.
+            InputStream is = url.openStream(); //url위치로 입력스트림 연결
+
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xpp = factory.newPullParser();
+            xpp.setInput(new InputStreamReader(is, "UTF-8")); //inputstream 으로부터 xml 입력받기
+
+            String tag;
+
+            xpp.next();
+            int eventType = xpp.getEventType();
+
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                switch (eventType) {
+                    case XmlPullParser.START_DOCUMENT:
+                        buffer.append("파싱 시작...\n\n");
+                        break;
+
+                    case XmlPullParser.START_TAG:
+                        tag = xpp.getName();//테그 이름 얻어오기
+
+                        if (tag.equals("dataTime")) { //title 만나면 내용을 받을수 있게 하자
+                            buffer.append("날짜 : ");
+                            xpp.next();
+                            buffer.append(xpp.getText());//title 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
+                        if (tag.equals("o3Value")) { //address 만나면 내용을 받을수 있게 하자
+                            buffer.append("오존 지수 : ");
+                            xpp.next();
+                            buffer.append(xpp.getText());//title 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
+                        if (tag.equals("pm10Value")) { //address 만나면 내용을 받을수 있게 하자
+                            buffer.append("미세먼지 수치 : ");
+                            xpp.next();
+                            buffer.append(xpp.getText());//title 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
+                        if (tag.equals("pm25Value")) { //address 만나면 내용을 받을수 있게 하자
+                            buffer.append("초미세먼지 수치 : ");
+                            xpp.next();
+                            buffer.append(xpp.getText());//title 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
+                        if (tag.equals("o3Grade")) { //address 만나면 내용을 받을수 있게 하자
+                            buffer.append("오존 수치 : ");
+                            xpp.next();
+                            buffer.append(xpp.getText());//title 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
+                        break;
+                    case XmlPullParser.TEXT:
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        tag = xpp.getName(); //테그 이름 얻어오기
+                        if (tag.equals("item")) buffer.append("\n");// 첫번째 검색결과종료..줄바꿈
+                        break;
+                }
+                eventType = xpp.next();
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch blocke.printStackTrace();
+        }
+        buffer.append("파싱 끝\n");
+        return buffer.toString();//StringBuffer 문자열 객체 반환
+    }
+    */
+}
+
---
+++ java/com/example/assortment/item.java
@@ -0,0 +1,81 @@
+package com.example.assortment;
+
+public class item {
+
+    String dataTime, o3Value, pm10Value, pm25Value, o3Grade, pm10Grade ;
+    String pm25Grade, pm10Grade1h, pm25Grade1h ;
+
+    public String getDataTime() {
+        return dataTime;
+    }
+
+    public void setDataTime(String dataTime) {
+        this.dataTime = dataTime;
+    }
+
+    public String getO3Value() {
+        return o3Value;
+    }
+
+    public void setO3Value(String o3Value) {
+        this.o3Value = o3Value;
+    }
+
+    public String getPm10Value() {
+        return pm10Value;
+    }
+
+    public void setPm10Value(String pm10Value) {
+        this.pm10Value = pm10Value;
+    }
+
+    public String getPm25Value() {
+        return pm25Value;
+    }
+
+    public void setPm25Value(String pm25Value) {
+        this.pm25Value = pm25Value;
+    }
+
+    public String getO3Grade() {
+        return o3Grade;
+    }
+
+    public void setO3Grade(String o3Grade) {
+        this.o3Grade = o3Grade;
+    }
+
+    public String getPm10Grade() {
+        return pm10Grade;
+    }
+
+    public void setPm10Grade(String pm10Grade) {
+        this.pm10Grade = pm10Grade;
+    }
+
+    public String getPm25Grade() {
+        return pm25Grade;
+    }
+
+    public void setPm25Grade(String pm25Grade) {
+        this.pm25Grade = pm25Grade;
+    }
+
+    public String getPm10Grade1h() {
+        return pm10Grade1h;
+    }
+
+    public void setPm10Grade1h(String pm10Grade1h) {
+        this.pm10Grade1h = pm10Grade1h;
+    }
+
+    public String getPm25Grade1h() {
+        return pm25Grade1h;
+    }
+
+    public void setPm25Grade1h(String pm25Grade1h) {
+        this.pm25Grade1h = pm25Grade1h;
+    }
+}
+
+
